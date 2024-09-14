704622e5827d4ab404866d8176bc0f70beb194b2
desy <desypuspita685@gmail.com>
2024-09-14 07:36:43 +0700

    commit jobsheet 3

---
+++ Listrik03.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Listrik03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int tarifListrik = 1500, jmlpenggunaan, totaltarifListrik;
+        boolean penggunaanListrik;
+
+        System.out.println("Masukkan Jumlah Penggunaan Listrik (kwh)\t\t: ");
+        jmlpenggunaan = sc.nextInt();
+
+        penggunaanListrik = jmlpenggunaan > 500;
+
+        totaltarifListrik = jmlpenggunaan * tarifListrik;
+
+        System.out.printf("\nOutput:\n\nApakah Listrik Melebihi 500 kWh?\t: %s\nJumlah Penggunaan Listrik\t\t\t\t: %s\nHarga Listrik (kWh)\t\t\t: %s\nTotal Bayar Listrik\t\t\t: %s", penggunaanListrik, jmlpenggunaan, tarifListrik, totaltarifListrik);
+    }    
+}
